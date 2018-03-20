6d256391e68e82243d4181b83b58cc6d5eb22b3a
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-20 14:37:23 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -25,6 +25,7 @@ attr_accessor :name, :type, :db, :id, :hp
   def self.find(num, db)
     pk = db.execute("SELECT * FROM pokemon WHERE id = ?", num).first
     self.new(id: num, name: pk[1], type: pk[2], db: db)
+    binding.pry
   end
 
 
